0185bdc4a29a56459ba043fc979bebcc5173353f
iqblfrdsyh <iqblfrdsyh@gmail.com>
2024-05-13 05:04:56 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, './api-docs')))
 app.use(fileUpload());
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
